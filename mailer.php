d894ce452727f50e2bbf52868e3eb3709a190342
AnhelinaRoz <rozkovanangelina@gmail.com>
2023-02-26 18:10:18 +0100

    ок

--- mailer.php
+++ mailer.php
@@ -3,9 +3,41 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $text = $_POST["text"];
 $to      = 'info@dreamsagency.net';
-$subject = 'the subject';
-$message = $text;
-$headers = 'From: ' . $email . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+// тема письма
+$subject = 'Birthday Reminders for August';
 
-mail($to, $subject, $message, $headers);
+// текст письма
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here are the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// Для отправки HTML-письма должен быть установлен заголовок Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Дополнительные заголовки
+$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+$headers[] = 'From: Birthday Reminder <birthday@example.com>';
+$headers[] = 'Cc: birthdayarchive@example.com';
+$headers[] = 'Bcc: birthdaycheck@example.com';
+
+// Отправляем
+mail($to, $subject, $message, implode("\r\n", $headers));
