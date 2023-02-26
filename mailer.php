089162f2738d7e6195820eaf472242b193f74047
AnhelinaRoz <rozkovanangelina@gmail.com>
2023-02-26 18:16:03 +0100

    ок

--- mailer.php
+++ mailer.php
@@ -3,41 +3,16 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $text = $_POST["text"];
 $to      = 'info@dreamsagency.net';
-// тема письма
-$subject = 'Birthday Reminders for August';
 
-// текст письма
-$message = '
-<html>
-<head>
-  <title>Birthday Reminders for August</title>
-</head>
-<body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
+$subject = 'Website Change Request';
 
-// Для отправки HTML-письма должен быть установлен заголовок Content-type
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers  = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+$headers .= "Reply-To: " . strip_tags($_POST['req-email']) . "\r\n";
+$headers .= "CC: susan@example.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-// Дополнительные заголовки
-$headers .=  'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
-$headers .=  'From: Birthday Reminder <birthday@example.com>';
-$headers .=  'Cc: birthdayarchive@example.com';
-$headers .=  'Bcc: birthdaycheck@example.com';
+$message = '<p><strong>This is strong text</strong> while this is not.</p>';
 
-// Отправляем
-mail($to, $subject, $message, implode("\r\n", $headers));
+
+mail($to, $subject, $message, $headers);
