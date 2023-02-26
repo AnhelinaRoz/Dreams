c8630911a21d6e9eb1aa319abab500005390cf83
AnhelinaRoz <rozkovanangelina@gmail.com>
2023-02-26 18:24:17 +0100

    sdafasdfasdf

--- mailer.php
+++ mailer.php
@@ -3,16 +3,13 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $text = $_POST["text"];
 $to      = 'info@dreamsagency.net';
+$subject = 'the subject';
+$message = 'hello';
 
-$subject = 'Website Change Request';
-
-$headers  = "From: " . $email. "\r\n";
-$headers .= "Reply-To: " . $email . "\r\n";
-$headers .= "CC: susan@example.com\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-$message = '<p><strong>This is strong text</strong> while this is not.</p>';
-
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
+
+
